79d05e9af44aeaba29cdac5e46ea8add46ba2fc0
Kohtla-Jarve <37462395+Kohtla-Jarve@users.noreply.github.com>
2018-04-10 15:14:00 -0400

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("I am adding code to may project");
+		System.out.println("I am adding code to my project");
+		System.out.println("I am adding code from GitHub");
 	}
 
 }
